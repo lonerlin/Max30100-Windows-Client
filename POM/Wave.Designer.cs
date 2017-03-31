1635d98f325dac5b7f7cf80a17efd1bfca132063
lin <vsnetlin@163.com>
2017-03-31 17:53:03 +0800

    添加Wave窗口

---
+++ POM/Wave.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace POM
+{
+    partial class Wave
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusChart1 = new UI.StatusChart();
+            this.SuspendLayout();
+            // 
+            // statusChart1
+            // 
+            this.statusChart1.BackColor = System.Drawing.Color.Black;
+            this.statusChart1.Enabled = false;
+            this.statusChart1.GridHeight = 50;
+            this.statusChart1.GridShiftting = false;
+            this.statusChart1.GridWidth = 50;
+            this.statusChart1.Interval = 10;
+            this.statusChart1.Location = new System.Drawing.Point(-1, 1);
+            this.statusChart1.Mode = UI.StatusChart.ChartMode.Waveform;
+            this.statusChart1.Name = "statusChart1";
+            this.statusChart1.Range = 500;
+            this.statusChart1.Size = new System.Drawing.Size(1008, 540);
+            this.statusChart1.TabIndex = 0;
+            // 
+            // Wave
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 541);
+            this.Controls.Add(this.statusChart1);
+            this.MaximizeBox = false;
+            this.Name = "Wave";
+            this.Text = "脉搏波形图";
+            this.Load += new System.EventHandler(this.Wave_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private UI.StatusChart statusChart1;
+    }
+}
---
+++ POM/Wave.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace POM
+{
+    public partial class Wave : Form
+    {
+
+        public SerialPort Port { get; set; }
+        public delegate void DrawWave(int value);
+
+        public void draw(int value)
+        {
+            this.statusChart1.Value = value;
+        }
+
+        DrawWave dWave;
+        public Wave()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Wave_Load(object sender, EventArgs e)
+        {
+           
+                
+                dWave = new DrawWave(this.draw);
+            
+        }
+
+        public void UpdateWave(float value)
+        {
+            Console.WriteLine(value);
+            this.statusChart1.Value = Convert.ToInt32(Math.Round(value))+150;
+        }
+
+       
+       
+
+      
+    }
+}
